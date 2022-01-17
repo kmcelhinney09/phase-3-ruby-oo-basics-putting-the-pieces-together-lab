c21d58eccd272eaf881547549976df00b3b3191d
mcelhinneyk <mr.mcelhinney@gmail.com>
2022-01-16 21:39:42 -0800

    commit tree def923547091f76bb6d95107dbda2b4d63596068

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+require 'pry'
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize (title, author="unknown", page_count=0, genre="unknown")
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
+# binding.pry
+# 0
